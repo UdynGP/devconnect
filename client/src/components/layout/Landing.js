6e0c35f498ea8b7050ad777181b20538af4a6d75
UdayanGupta <udayangupta97@gmail.com>
2021-11-18 11:03:52 +0530

    React setup complete

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Welcome to DevConnect!</h1>
+          <p className='lead'>
+            Create a developer profile/portfolio, share posts and connect
+            globally with other developers
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,68 +12,68 @@ const User = require('../../models/User');
 // @desc     Test route
 // @access   Public
 router.get('/', auth, async (req, res) => {
-	try {
-		const user = await User.findById(req.user.id).select('-password');
-		res.json(user);
-	} catch (err) {
-		console.err(err.message);
-		res.status(500).send('Server error');
-	}
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
-// @route    POS api/auth
+// @route    POST api/auth
 // @desc     Authenticate user & Get token
 // @access   Public
 router.post(
-	'/',
-	[
-		check('email', 'Please include a valid email id').isEmail(),
-		check('password', 'Password is required').exists(),
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
+  '/',
+  [
+    check('email', 'Please include a valid email id').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-		const { email, password } = req.body;
+    const { email, password } = req.body;
 
-		try {
-			let user = await User.findOne({ email });
+    try {
+      let user = await User.findOne({ email });
 
-			if (!user) {
-				return res
-					.status(400)
-					.json({ errors: [{ msg: 'Invalid Credentials' }] });
-			}
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
 
-			const isMatch = await bcrypt.compare(password, user.password);
-			if (!isMatch) {
-				return res
-					.status(400)
-					.json({ errors: [{ msg: 'Invalid Credentials' }] });
-			}
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
 
-			const payload = {
-				user: {
-					id: user.id,
-				},
-			};
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-			jwt.sign(
-				payload,
-				config.get('jwtToken'),
-				{ expiresIn: 360000 },
-				(err, token) => {
-					if (err) throw err;
-					res.json({ token });
-				},
-			);
-		} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server error');
-		}
-	},
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
 module.exports = router;
