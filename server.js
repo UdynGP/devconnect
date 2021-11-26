12add999a690e9f5f127b46770e7f17b600c4d4f
Udayan Gupta <62198309+UdynGP@users.noreply.github.com>
2021-11-26 20:50:19 +0530

    Updated code in server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === production) {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
